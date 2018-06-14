40ab37656698d34359402cc9c4e362d9af2f2fa0
Sureshkumar Linganathan <sureshkumarlinganathan@suresh-kumar.local>
2018-06-14 10:25:19 +0530

    Initial Commit

---
+++ Truckish/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Truckish
+//
+//  Created by Sureshkumar Linganathan on 6/14/18.
+//  Copyright © 2018 Truckish. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
